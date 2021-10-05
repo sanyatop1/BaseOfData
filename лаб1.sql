19f17c40a4dd3df6df211efbb4642e16d5a97e05
sanyatop1 <71902164+sanyatop1@users.noreply.github.com>
2021-10-05 15:54:30 +0300

    Add files via upload

---
+++ лаб1.sql
@@ -0,0 +1,17 @@
+﻿-- Table: public."Books"
+
+-- DROP TABLE public."Books";
+
+CREATE TABLE public."Books"
+(
+  "BookId" integer NOT NULL DEFAULT nextval('"Books_BookId_seq"'::regclass),
+  "BookName" character varying(25),
+  "BookAuthor" character varying(25),
+  "BookUser" character varying(25),
+  CONSTRAINT "BookIdPK" PRIMARY KEY ("BookId")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public."Books"
+  OWNER TO postgres;
